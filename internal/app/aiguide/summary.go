c84ba806f5d2d853eaa509802b35abcde6577905
薛运成 <datouguix@gmail.com>
2025-12-20 17:00:34 +0800

    Refactor SearchAgent and SummaryAgent configurations for clarity and completeness

--- internal/app/aiguide/summary.go
+++ internal/app/aiguide/summary.go
@@ -8,6 +8,8 @@ import (
 	"google.golang.org/adk/agent/llmagent"
 	"google.golang.org/adk/agent/workflowagents/sequentialagent"
 	"google.golang.org/adk/model"
+	"google.golang.org/adk/tool"
+	"google.golang.org/adk/tool/geminitool"
 )
 
 func NewSequentialAgent(model model.LLM) (agent.Agent, error) {
@@ -39,11 +41,15 @@ func NewSequentialAgent(model model.LLM) (agent.Agent, error) {
 
 func NewSearchAgent(model model.LLM) (agent.Agent, error) {
 	searchAgentConfig := llmagent.Config{
-		Name:        "SearchAgent",
-		Model:       model,
-		Instruction: `你是一个搜索机器人，你会根据用户的查询内容，去网络上搜索相关的信息，并将搜索结果返回给用户。请确保你的回答简洁明了，直接切中要点。`,
-		Description: "",
+		Name:  "SearchAgent",
+		Model: model,
+		Instruction: `你是一个搜索机器人，用户会让你介绍某个人的观点。
+你需要去网络上搜索这个人的所有公开演讲和文章。
+根据用户提供的名字，你需要在网络上搜索相关的信息，并返回给用户。
+`,
+		Description: "这个 agent 可以根据用户查询的内容去网络上查找这个人所有的演讲和文章",
 		OutputKey:   "search_agent_output",
+		Tools:       []tool.Tool{geminitool.GoogleSearch{}},
 	}
 	agent, err := llmagent.New(searchAgentConfig)
 	if err != nil {
@@ -59,7 +65,7 @@ func NewSummaryAgent(model model.LLM) (agent.Agent, error) {
 		Name:        "SummaryAgent",
 		Model:       model,
 		Instruction: `你是一个总结机器人，你会根据用户提供的内容，生成一个简洁明了的总结。请确保你的总结涵盖所有重要信息，并且易于理解。`,
-		Description: "",
+		Description: "这个 agent 可以根据用户提供的内容，生成一个简洁明了的总结",
 		OutputKey:   "summary_agent_output",
 	}
 	agent, err := llmagent.New(summaryAgent)
